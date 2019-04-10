3ad8fff3f06a893d8565604246c0061380c5cfbc
bridgream <22215950+bridgream@users.noreply.github.com>
2019-04-10 11:35:15 +0800

    Add new functions, structure update

--- mylib/backtest.py
+++ mylib/backtest.py
@@ -1,7 +1,7 @@
 """
 Name: Back test engine
 Contributor: Gu Chengyang
-Date: 2019/4/3
+Date: 2019/4/10
 Objective: 基于用户指定策略的回测
 Run successfully on macOS 10.14.4, Python 3.7.2
 
@@ -12,9 +12,12 @@ Directions: 用户在调用的时候，
 用户需要注意的是，self.strategy() 函数每天都会被重新调用，且不会保留该日之前存储的变量。
 如您的策略需要涉及与该日之前的数据有关的结果，请将其设置为实例属性（即：使用 self.your_attribute）。
 对于不熟悉语法的用户，凡名称前面有双下划线的属性和方法，均为私有方法。请调用不带下划线的属性和方法。
+
+非自由
 """
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import seaborn as sns
@@ -38,14 +41,27 @@ register_matplotlib_converters()
 
 def security_check(f):
     """
-    检查传入资产名称的装饰器
+    检查传入资产名称是否合法，以及价格是否为 NaN 的装饰器
     :param f:
     :return:
     """
 
     @wraps(f)
     def wrapper(self, security, *args, **kwargs):
+        if isinstance(security, str):
+            if security not in self.securities:
+                raise TypeError(f'{security}: security not defined')
+            if isnan(self.price.loc[self.today, security]):
+                raise ValueError(f'price for security {security} on {self.today} is NaN')
+
+        elif isinstance(security, list):
+            pass
+
+        else:
+            raise TypeError('security unrecognized')
+
         f(self, security, *args, **kwargs)
+
     return wrapper
 
 
@@ -60,16 +76,26 @@ class FuturesMultiplier(object):
 
     class Dalian(Enum):
         pass
-        
+
     class CFFEX(Enum):
-        pass
+        IF300 = 300
+        IF = 300
+        IH50 = 300
+        IH = 300
+        IC500 = 200
+        IC = 200
+        ETF = 10000
+        CFFEX = 10000
 
     class SHOil(Enum):
         pass
 
 
 class BackTestFramework(object):
+    # debug 模式，打印每笔交易明细
     print_debugging = False
+
+    # 详细的 debug 模式，打印每个迭代到的日期，需要先启动 debug 模式
     print_debugging_detail = False
 
     def __init__(self, **kwargs):
@@ -82,57 +108,57 @@ class BackTestFramework(object):
     # securities
     @property
     def securities(self):
-        return self.__securities
+        return self._securities
 
     # stock_list
     @property
     def stock_list(self):
-        return self.__stock_list
+        return self._stock_list
 
     # futures_list
     @property
     def futures_list(self):
-        return self.__futures_list
+        return self._futures_list
 
     # benchmark
     @property
     def benchmark(self):
-        return self.__benchmark
+        return self._benchmark
 
     # factors
     @property
     def factors(self):
-        return self.__factors
-
+        return self._factors
+    
     # start_date
     @property
     def start_date(self):
-        return self.__start_date
+        return self._start_date
 
     # end_date
     @property
     def end_date(self):
-        return self.__end_date
+        return self._end_date
 
     # 税费
     @property
     def commission(self):
-        return self.__commission
+        return self._commission
 
     # 滑点
     @property
     def slippage(self):
-        return self.__slippage
+        return self._slippage
 
     # 初始现金
     @property
     def cash(self):
-        return self.__cash
+        return self._cash
 
     # 价格矩阵
     @property
     def price(self):
-        return self.__price
+        return self._price
 
     @price.setter
     def price(self, value: pd.DataFrame):
@@ -146,38 +172,43 @@ class BackTestFramework(object):
     # 日志，只读
     @property
     def log(self):
-        return self.__log
+        return self._log
 
     # 每日持仓量，只读
     @property
     def position(self):
-        return self.__position
+        return self._position
 
     # 当前持仓量，只读
     @property
     def current_position(self):
-        return self.__current_position
+        return self._current_position
 
     # 每日价值，只读
     @property
     def value(self):
-        return self.__value
+        return self._value
+
+    # 因手续费支出的现金，只读
+    @property
+    def cash_spent_on_commission(self):
+        return self._cash_spent_on_commission
 
     # 今日日期，只读
     @property
     def today(self):
-        if self.__today is None:
+        if self._today is None:
             warn('no today available, None is returned')
-        return self.__today
+        return self._today
 
     # 昨日日期，只读
     @property
     def yesterday(self):
-        if self.__yesterday is None:
+        if self._yesterday is None:
             warn('no yesterday available, None is returned')
-        return self.__yesterday
+        return self._yesterday
 
-    def __logger(self, security, volume_change, date):
+    def _logger(self, security, volume_change):
         """
         记录日志的函数
         :param security: 交易资产名类
@@ -186,28 +217,18 @@ class BackTestFramework(object):
         """
         pass
 
-    def __trade_classifier(self, f, security, *args, **kwargs):
+    def _trade_classifier(self, f, security, *args, **kwargs):
         """
-        区分传入的是单个资产名称还是一个 tuple，然后分别进行交易
+        区分传入的是单个资产名称还是一个 list，然后分别进行交易
         :param f: 交易调用的函数
         :param security: 传入资产
         :param args:
         :param kwargs:
         :return:
         """
-        if isinstance(security, str):
-            # single security
-            if security in self.stock_list:
-                f(security, *args, **kwargs)
-        else:
-            # a tuple of securities
-            for s in security:
-                if s in self.stock_list:
-                    f(security, *args, **kwargs)
-                if s in self.futures_list:
-                    pass
-        
-    def __order_single(self, security, volume_change):
+        pass
+
+    def _order_single(self, security, volume_change):
         """
         单个资产下单函数，这个函数是主交易函数，所有的交易都需要它来完成，交易日志的记录也由它调用
         :param security: 交易资产名类，单个
@@ -217,72 +238,82 @@ class BackTestFramework(object):
         pass
 
     @security_check
-    def buy(self, security: str or tuple, volume):
+    def buy(self, security: str or list, volume):
         """
         下单加仓函数
-        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 tuple
+        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 list
         :param volume: 交易数量
         :return: no return
         """
         pass
 
     @security_check
-    def sell(self, security: str or tuple, volume):
+    def sell(self, security: str or list, volume):
         """
         下单减仓函数
-        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 tuple
+        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 list
         :param volume: 交易数量
         :return: no return
         """
         pass
 
-    def __adjust_to_single(self, security, new_position):
+    def _adjust_to_single(self, security, new_position):
         pass
 
     @security_check
-    def adjust_to(self, security: str or tuple, new_position):
+    def adjust_to(self, security: str or list, new_position):
         """
         将某资产的仓位直接调整到所需的大小
-        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 tuple
+        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 list
         :param new_position: 持仓目标数量
         :return:
         """
         pass
 
+    def _adjust_to_portion_of_value_single(self, security, portion_of_value):
+        pass
+
     @security_check
-    def adjust_to_portion_of_value(self, security: str or tuple, portion_of_value):
+    def adjust_to_portion_of_value(self, security: str or list, portion_of_value):
         """
         将某资产的仓位直接调整到组合的价值比例
         例如，portion_of_value = 0.2 意味着把仓位调整到资产价值总的20%，注意受到剩余现金的约束
-        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 tuple
+        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 list
         :param portion_of_value: 持仓目标与目前的比例
         :return:
         """
         pass
 
-    def __adjust_to_by_portion_single(self, security, portion, otherwise_position):
+    def _adjust_to_by_portion_single(self, security, portion, otherwise_position):
         pass
 
     @security_check
-    def adjust_to_by_portion(self, security: str or tuple, portion, otherwise_position=0):
+    def adjust_to_by_portion(self, security: str or list, portion, otherwise_position=0):
         """
         将某资产的仓位按现在的比例调整到所需的大小
-        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 tuple
+        :param security: 交易资产名类，可以是单个的名称（字符串），也可以是 list
         :param portion: 持仓目标与目前的比例
         :param otherwise_position: 如果现在持仓量为 0，可以同时调整到一个需要的仓位
         :return:
         """
         pass
 
-    def __initialize(self):
+    def _initialize(self):
         """
         初始化函数，根据用户指定的资产类型生成空的持仓量矩阵
         :return: no return
         """
         pass
 
+    def _finalize(self):
+        """
+        最后处理函数，生成每日动态价值的矩阵
+        :return:
+        """
+        pass
+
     @staticmethod
-    def __date_range(start_date, end_date):
+    def _date_range(start_date, end_date):
         """
         this generator allows iteration over days, skipping the first day
         :param start_date:
@@ -311,10 +342,13 @@ class BackTestFramework(object):
         启动
         :return: no return
         """
-        self.__initialize()
-        for single_date in self.__date_range(self.start_date, self.end_date):
+        self._initialize()
+
+        for single_date in self._date_range(self.start_date, self.end_date):
             if single_date in self.price.index:
                 pass
+
+        self._finalize()
 
     def summary(self, save_pic=False, pic_name='image.png'):
         """
